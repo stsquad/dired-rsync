f8e0f15e60f2cffc4adf76bfc16431a34b61c4f8
Alex Bennée <alex.bennee@linaro.org>
2018-04-13 15:26:51 +0100

    docs: move historical commentary to README file and re-write header

--- dired-rsync.el
+++ dired-rsync.el
@@ -25,14 +25,18 @@
 ;;
 ;;; Commentary:
 ;;
-;; There have been several attempts at doing this but I found them
-;; wanting in usability. This attempts to clean up the ideas from:
+;; dired-rsync is a command that can be run from a dired buffer to
+;; copy files using rsync rather than tramps in-built mechanism. This
+;; is especially useful for copying large files to/from remote
+;; locations without locking up tramp.
 ;;
-;;   https://truongtx.me/tmtxt-dired-async.html
-;;   https://oremacs.com/2016/02/24/dired-rsync/
-;;   https://vxlabs.com/2018/03/30/asynchronous-rsync-with-emacs-dired-and-tramp/
+;; To use simply open a dired buffer, mark some files and invoke
+;; dired-rsync. After being prompted for a location to copy to an
+;; inferior rsync process will be spawned.
 ;;
-;; in a hopefully cleaner and more idiomatic way.
+;; Wherever the files are selected from the rsync will always run from
+;; your local machine.
+;;
 
 (require 'dired-aux) ; for dired-dwim-target-directory
 (require 'dash)
