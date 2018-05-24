643d140bbf18f8af1fb2022a7ac1f9e4058123b9
Daniel Kraus <daniel@kraus.my>
2018-05-24 11:42:10 +0200

    Fix commentary according to checkdoc
    2 spaces are required after a dot.

--- dired-rsync.el
+++ dired-rsync.el
@@ -26,12 +26,12 @@
 ;;; Commentary:
 ;;
 ;; dired-rsync is a command that can be run from a dired buffer to
-;; copy files using rsync rather than tramps in-built mechanism. This
-;; is especially useful for copying large files to/from remote
+;; copy files using rsync rather than tramps in-built mechanism.
+;; This is especially useful for copying large files to/from remote
 ;; locations without locking up tramp.
 ;;
 ;; To use simply open a dired buffer, mark some files and invoke
-;; dired-rsync. After being prompted for a location to copy to an
+;; dired-rsync.  After being prompted for a location to copy to an
 ;; inferior rsync process will be spawned.
 ;;
 ;; Wherever the files are selected from the rsync will always run from
