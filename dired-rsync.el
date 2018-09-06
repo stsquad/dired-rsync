e112bf22c913d1491bb2149250be866ceb1806ff
Alex Bennée <alex.bennee@linaro.org>
2018-09-06 11:02:00 +0100

    dired-rsync: document type of dest it can take (FR #9)
    This seems a harmless enough improvement. Tested with remote ssh/scp
    tramp targets.

--- dired-rsync.el
+++ dired-rsync.el
@@ -194,7 +194,10 @@ dired-buffer modeline."
 
 ;;;###autoload
 (defun dired-rsync (dest)
-  "Asynchronously copy files in dired to DEST using rsync.
+  "Asynchronously copy files in dired to `DEST' using rsync.
+
+`DEST' can be a relative filename and will be processed by
+`expand-file-name' before being passed to the rsync command.
 
 This function runs the copy asynchronously so Emacs won't block whilst
 the copy is running.  It also handles both source and destinations on
@@ -202,6 +205,8 @@ ssh/scp tramp connections."
   ;; Interactively grab dest if not called with
   (interactive
    (list (read-file-name "rsync to:" (dired-dwim-target-directory))))
+
+  (setq dest (expand-file-name dest))
 
   (let ((src-files (-map
                     'dired-rsync--quote-and-maybe-convert-from-tramp
