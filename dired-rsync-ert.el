fe5988154c843c70d080ae8a822f27ed83983d01
charl botha <cpbotha+x1e-manj@vxlabs.com>
2020-05-08 23:19:58 +0200

    Add tests for quote-and-maybe-convert-from-tramp

--- dired-rsync-ert.el
+++ dired-rsync-ert.el
@@ -57,6 +57,13 @@
                         (car (dired-rsync--extract-paths-from-tramp
                               '("/ssh:servername|sudo:root@servername:/path/to/file.txt"))))))
 
+(ert-deftest dired-rsync-test-quote-and-maybe-convert-from-tramp ()
+  "Test quote and maybe convert from tramp defun"
+  ;; test against regression of issue #26: missing username in rsync command
+  (should (string-equal "username@192.168.1.1:\"/blat/blot/\""
+                        (dired-rsync--quote-and-maybe-convert-from-tramp "/scp:username@192.168.1.1:/blat/blot/")))
+  (should (string-equal "192.168.1.1:\"/blat/blot/\""
+                        (dired-rsync--quote-and-maybe-convert-from-tramp "/scp:192.168.1.1:/blat/blot/"))))
 
 (ert-deftest dired-rsync-test-remote-port()
   "Test the remote port handling."
