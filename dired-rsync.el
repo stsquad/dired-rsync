f8a654d84389b5dc841eb087905e5e67f4d206f9
Aaron L. Zeng <me@bcc32.com>
2026-07-07 00:04:41 -0400

    Tweak formatting in Commentary section

--- dired-rsync.el
+++ dired-rsync.el
@@ -25,16 +25,16 @@
 ;;
 ;;; Commentary:
 ;;
-;; dired-rsync is a command that can be run from a Dired buffer to
-;; copy files using rsync rather than tramps in-built mechanism.
-;; This is especially useful for copying large files to/from remote
-;; locations without locking up tramp.
+;; `dired-rsync' is a command that can be run from a Dired buffer to
+;; copy files using rsync rather than TRAMP's built-in mechanism.  This
+;; is especially useful for copying large files to/from remote locations
+;; without locking up TRAMP.
 ;;
 ;; To use simply open a Dired buffer, mark some files and invoke
-;; dired-rsync.  After being prompted for a location to copy to an
+;; `dired-rsync'.  After being prompted for a location to copy to, an
 ;; inferior rsync process will be spawned.
 ;;
-;; Wherever the files are selected from the rsync will always run from
+;; Wherever the files are selected from, the rsync will always run from
 ;; your local machine.
 ;;
 
